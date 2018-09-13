f3b1e79d3976e983c8db3b0d86bd34c1097e4be4
TES-NB-130\Olexandr Korsun <bersh92@gmail.com>
2018-09-13 16:40:39 +0200

    add new test examples

---
+++ src/test/java/src/TestClass2.java
@@ -0,0 +1,25 @@
+package src;
+
+public class TestClass2 <T> {
+
+    private T id;
+
+    private T number;
+
+    public T getId() {
+        return id;
+    }
+
+    public void setId(T id) {
+        this.id = id;
+    }
+
+    public T getNumber() {
+        return number;
+    }
+
+    public void setNumber(T number) {
+        this.number = number;
+    }
+
+}
--- src/test/java/src/TestJavaTraining.java
+++ src/test/java/src/TestJavaTraining.java
@@ -3,6 +3,7 @@ package src;
 public class TestJavaTraining extends TestClass1 {
     public static void main(String[] args) {
         TestJavaTraining testObj = new TestJavaTraining();
+        TestClass2 <Integer> testObj1 = new TestClass2 <Integer> (); // Generic
         TestClass1 obj = new TestClass1(8); // use custom constructor from TestClass1 class
         testObj.test_for_loop(); // can call method from TestClass1 using obj of TestJavaTraining class
         testObj.test_map();
@@ -10,6 +11,8 @@ public class TestJavaTraining extends TestClass1 {
         testObj.setNum(56);
         TestClass1.testOverload("parameter");
         testObj.getValue();
+        testObj1.setNumber(5); //Generic
+
     }
 
 }
